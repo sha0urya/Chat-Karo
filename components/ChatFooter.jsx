c4b1c4c28992421af04588557ab5e4ac4f845c96
Praveen Kumar <singhsatyam312005@gmail.com>
2024-07-29 02:50:01 +0530

    placeholder

--- components/ChatFooter.jsx
+++ components/ChatFooter.jsx
@@ -42,80 +42,78 @@ const ChatFooter = () => {
     };
 
     return (
-        <div className="flex items-center bg-c1/[0.5] p-2 rounded-xl relative">
-            {attachmentPreview && (
-                <div className="absolute w-[100px] h-[100px] bottom-16 left-0 bg-c1 p-2 rounded-md">
-                    <Image src={attachmentPreview} alt="" />
-                    <div
-                        className="w-6 h-6 rounded-full bg-red-500 flex justify-center items-center absolute -right-2 -top-2 cursor-pointer"
-                        onClick={() => {
-                            setAttachment(null);
-                            setAttachmentPreview(null);
-                        }}
-                    >
-                        <MdDeleteForever size={14} />
-                    </div>
-                </div>
-            )}
-            <div className="shrink-0">
-                <input
-                    type="file"
-                    id="fileUploader"
-                    className="hidden"
-                    onChange={onFileChange}
-                />
-                <label htmlFor="fileUploader">
-                    <Icon
-                        size="large"
-                        icon={<CgAttachment size={20} className="text-c3" />}
-                    />
-                </label>
+      <div className="flex items-center bg-c1/[0.5] p-2 rounded-xl relative">
+        {attachmentPreview && (
+          <div className="absolute w-[100px] h-[100px] bottom-16 left-0 bg-c1 p-2 rounded-md">
+            <Image src={attachmentPreview} alt="" />
+            <div
+              className="w-6 h-6 rounded-full bg-red-500 flex justify-center items-center absolute -right-2 -top-2 cursor-pointer"
+              onClick={() => {
+                setAttachment(null);
+                setAttachmentPreview(null);
+              }}
+            >
+              <MdDeleteForever size={14} />
             </div>
-
-            <div className="shrink-0 relative">
-                <Icon
-                    size="large"
-                    className={``}
-                    icon={<HiOutlineEmojiHappy size={24} className="text-c3" />}
-                    onClick={() => setShowImojiPicker(true)}
-                />
-                {showImojiPicker && (
-                    <ClickAwayListener
-                        onClickAway={() => setShowImojiPicker(false)}
-                    >
-                        <div className="absolute bottom-12 left-0 shadow-lg">
-                            <EmojiPicker
-                                emojiStyle="native"
-                                theme="light"
-                                onEmojiClick={onEmojiClick}
-                                autoFocusSearch={false}
-                            />
-                        </div>
-                    </ClickAwayListener>
-                )}
-            </div>
-
-            {isTyping && (
-                <div className="absolute -top-6 left-4 bg-c2 w-full h-6">
-                    <div className="flex gap-2 w-full h-full opacity-50 text-sm text-white">
-                        {`${data?.user?.displayName} is typing`}
-                        <Image src="/typing.svg" alt="" />
-                    </div>
-                </div>
-            )}
-
-            {editMsg && (
-                <div
-                    className="absolute -top-12 left-1/2 -translate-x-1/2 bg-c4 flex items-center gap-2 py-2 px-4 pr-2 rounded-full text-sm font-semibold cursor-pointer shadow-lg"
-                    onClick={() => setEditMsg(null)}
-                >
-                    <span>Cancel edit</span>
-                    <IoClose size={20} className="text-white" />
-                </div>
-            )}
-
-            <Composebar />
+          </div>
+        )}
+        <div className="shrink-0">
+          <input
+            type="file"
+            id="fileUploader"
+            className="hidden"
+            onChange={onFileChange}
+          />
+          <label htmlFor="fileUploader">
+            <Icon
+              size="large"
+              icon={<CgAttachment size={20} className="text-c3" />}
+            />
+          </label>
         </div>
+
+        <div className="shrink-0 relative">
+          <Icon
+            size="large"
+            className={``}
+            icon={<HiOutlineEmojiHappy size={24} className="text-c3" />}
+            onClick={() => setShowImojiPicker(true)}
+          />
+          {showImojiPicker && (
+            <ClickAwayListener onClickAway={() => setShowImojiPicker(false)}>
+              <div className="absolute bottom-12 left-0 shadow-lg">
+                <EmojiPicker
+                  emojiStyle="native"
+                  theme="light"
+                  onEmojiClick={onEmojiClick}
+                  autoFocusSearch={false}
+                />
+              </div>
+            </ClickAwayListener>
+          )}
+        </div>
+
+        {isTyping && (
+          <div className="absolute -top-6 left-4 bg-c2 w-full h-6">
+            <div className="flex gap-2 w-full h-full opacity-50 text-sm text-white">
+              {`${data?.user?.displayName} is typing`}
+              <Image src="/typing.svg" alt="" width="36" height="36" />
+            </div>
+          </div>
+        )}
+
+        {editMsg && (
+          <div
+            className="absolute -top-12 left-1/2 -translate-x-1/2 bg-c4 flex items-center gap-2 py-2 px-4 pr-2 rounded-full text-sm font-semibold cursor-pointer shadow-lg"
+            onClick={() => setEditMsg(null)}
+          >
+            <span>Cancel edit</span>
+            <IoClose size={20} className="text-white" />
+          </div>
+        )}
+
+        <Composebar />
+      </div>
     );
 };
 
--- components/Loader.jsx
+++ components/Loader.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-
 import Image from "next/image";
 
-const Loader = () => {
-    return (
-        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center">
-            <Image
-                src="/loader.svg"
-                alt="Loading..."
-                width={100}
-                height={100}
-            />
-        </div>
-    );
+const Loader = ({ width = 800, height = 800 }) => {
+  return (
+    <div className="w-full h-full flex flex-col p-2 items-center justify-center bg-brand-secondary">
+      <Image
+        src="/loading_state.gif"
+        alt="Loading..."
+        width={width}
+        height={height}
+      />
+      <h2 className="text-brand-primary text-4xl mt-[-40px]">Loading . . .</h2>
+    </div>
+  );
 };
 
 export default Loader;
